8deb31d37018fd8e6f784669e455484d5b0118c9
Gabriel Tunggala <gtunggala@gmail.com>
2019-07-19 16:56:03 -0400

    added mongo url

--- create-react-express/server.js
+++ create-react-express/server.js
@@ -2,6 +2,22 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const MONGODBI_URI =
+  process.env.MONGODBI_URI ||
+  "mongodb://heroku_ph1n9006:c5v3ru1i6ndf8kkj2qfdap69pf@ds151997.mlab.com:51997/heroku_ph1n9006";
+mongoose.connect("mongodb://localhost/googlebooks", { useNewUrlParser: true });
+var Schema = mongoose.Schema;
+
+var bookSchema = new Schema({
+  title: String,
+  authors: Array,
+  description: String,
+  image: String,
+  link: String
+});
+
+const Book = mongoose.model("Book", bookSchema);
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +28,26 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.get("/api/books", function(req, res) {
+  Book.find({}, function(err, books) {
+    if (err) {
+      res.sendStatus(500);
+      return;
+    }
+    return res.json(books);
+  });
+});
+
+app.post("/api/books", function(req, res) {
+  Book.create(req.body);
+  return res.sendStatus(201);
+});
+
+app.delete("/api/books", function(req, res) {
+  Book.findByIdAndRemove(req.params.id, function() {
+    return res.sendStatus(204);
+  });
+});
 
 // Send every other request to the React app
 // Define any API routes before this runs
